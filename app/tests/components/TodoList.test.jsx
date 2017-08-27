9b16c43dc7c22d35e1bbe7906e6d7875f81dc1f7
Stanley Phu <sphu@ucsd.edu>
2017-08-27 13:50:49 -0700

    Add showCompleted and searchText reducers

--- app/tests/components/TodoList.test.jsx
+++ app/tests/components/TodoList.test.jsx
@@ -35,6 +35,6 @@ describe('TodoList', () => {
     var todoList = TestUtils.renderIntoDocument(<TodoList todos={todos}/>);
     var $el = $(ReactDOM.findDOMNode(todoList));
 
-    expect($el.find('.container__message').length).toBe(1);
+    expect($el.find('.container__message').length).toBe(0);
   });
 });
